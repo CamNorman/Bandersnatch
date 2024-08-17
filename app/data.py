313877e20beded27684d3b5ebb91fcb6ffe11119
CamNorman <142457053+CamNorman@users.noreply.github.com>
2024-08-17 12:49:42 -0700

    Update data.py
    Added comments describing what each function was doing

--- app/data.py
+++ app/data.py
@@ -13,6 +13,7 @@ class Database:
         self.database = MongoClient(getenv('DB_URL'), tlsCAFile=where())["Collection"]
         self.collection = self.database.get_collection('Monsters')
 
+# Insert any amount of moonsters into data
     def seed(self, amount=1000):
         monsters = []
         for _ in range(amount):
@@ -20,17 +21,21 @@ class Database:
         self.collection.insert_many(monsters)
         return f"{amount} Monsters created"
 
+# Erase all previous monsters from collection
     def reset(self):
         self.collection.delete_many({})
         return "Monsters Reset"
 
+# Count amount of monsters in collection
     def count(self) -> str:
         number_of_monsters = self.collection.count_documents({})
         return f"You have {number_of_monsters} Monsters"
 
+# Convert collection into a dataframe
     def dataframe(self) -> DataFrame:
         return DataFrame(self.collection.find({}, {"_id":False}))
 
+# Convert collection into html format
     def html_table(self) -> str:
         return self.dataframe().to_html(index=False)
 
